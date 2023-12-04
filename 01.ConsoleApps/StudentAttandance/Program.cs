34e5198e2108fc9f63ab5801b9f69dbb468fa1b4
Muhammet Furkan Ayhan <mfurkana067@gmail.com>
2023-12-04 14:03:34 +0300

    Added StudentAttendance (Homework Solution)

---
+++ 01.ConsoleApps/StudentAttandance/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace StudentAttendance;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // string[] students = new string[4]; //array
+        List<Student> students = new()
+        {
+            new Student()
+            {
+                No = 1,
+                Name = "Muhammet Furkan Ayhan",
+                Class = "1A",
+                DateOfBirth = Convert.ToDateTime("23.05.1995"),
+            },
+            new Student()
+            {
+                No = 2,
+                Name = "Seda Yavuz Ayhan",
+                Class = "1A",
+                DateOfBirth = Convert.ToDateTime("10.06.1995")
+            }
+        };
+
+        var student = new Student()
+        {
+            No = 3,
+            Name = "Yusuf Emre Ayhan",
+            Class = "1A",
+            DateOfBirth = Convert.ToDateTime("10.06.1995")
+        };
+
+        students.Add(student);
+
+
+        Console.WriteLine("Hi, this is my students!");
+
+        foreach (var s in students)
+        {
+            /*Console.WriteLine($@"
+                Number: {s.No}
+                Name: {s.Name}
+                Class: {s.Class}
+                Date Of Birth: {s.DateOfBirth}
+                --------------------");
+            */
+            Console.WriteLine($"{s.No}. {s.Name}");
+        }
+
+        while (true)
+        {
+            // Check if all students are in class.
+            foreach (var s in students)
+            {
+                if (!s.IsInClass)
+                {
+                    goto start;
+                }
+            }
+
+            Console.WriteLine("All students are in class!");
+            break;
+
+        start:;
+            Console.WriteLine("Tell me who is in class?");
+            string noString = Console.ReadLine();
+            int no = 0;
+            // Check if the input is numeric.
+            bool result = int.TryParse(noString, out no);
+            if (!result)
+            {
+                Console.WriteLine("Please write a number!");
+                continue;
+            }
+
+            // Check if there is a student with the entered number.
+            result = students.Any(x => x.No == no);
+            if (!result)
+            {
+                Console.WriteLine("The number you entered doesn't belong to our class. Please try again!");
+                continue;
+            }
+
+            // Check if the student with the entered number is already marked as present in class.
+            Student st = students.FirstOrDefault(p => p.No == no);
+            if (st.IsInClass)
+            {
+                Console.WriteLine("This student already in class!");
+                continue;
+            }
+
+            // Mark the student as present in class.
+            st.IsInClass = true;
+            Console.WriteLine($"{st.Name} marked as present in class.");
+        }
+    }
+}
+
+// Access Modifier
+internal class Student
+{
+    public int No { get; set; }
+    public string Name { get; set; } // property, TitleCase
+    public string Class { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public bool IsInClass { get; set; } = false;
+}
---
+++ 01.ConsoleApps/StudentAttandance/README.md
@@ -0,0 +1,38 @@
+# Student Attendance
+
+This is a simple console application that manages student attendance. The program allows users to mark students present in the class and provides basic functionalities for checking attendance.
+
+## Features
+
+- Display a list of students with their assigned numbers.
+- Mark students as present in the class.
+- Check if all students are present.
+- Bonus Task: Avoid marking a student present if they are already marked, and throw an error.
+- Bonus Task: If all students are marked present, the application should say "All students are present in class" and exit.
+
+## How to Use
+
+1. Clone or download the repository.
+2. Open the project in your preferred C# development environment.
+3. Run the `Program.cs` file to start the console application.
+4. Follow the prompts to interact with the student attendance system.
+
+## Student Class
+
+The program uses a `Student` class to represent each student with the following properties:
+
+- `No`: Student number.
+- `Name`: Student name.
+- `Class`: Student class.
+- `DateOfBirth`: Student date of birth.
+- `IsInClass`: Boolean indicating whether the student is present in class.
+
+## Contributors
+
+- [@M.Furkan Ayhan](https://github.com/mfurkanayhan)
+
+Feel free to contribute to this project by adding more features or improving existing functionalities.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
