d38ab2b4f65b973224f48036ba32e9616c1e6a6a
Muhammet Furkan Ayhan <mfurkana067@gmail.com>
2024-07-22 10:56:38 +0300

    Qick/QickServer: Infrastructure layer added

---
+++ 09.Projects/Qick/QickServer/QickServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QickServer.Domain.Users;
+
+namespace QickServer.Infrastructure.Context;
+internal sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {}
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ 09.Projects/Qick/QickServer/QickServer.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QickServer.Infrastructure.Context;
+using Scrutor;
+
+namespace QickServer.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("SqlServer")!;
+
+        services.AddDbContext<ApplicationDbContext>(conf =>
+        {
+            conf.UseSqlServer(connectionString);
+        });
+
+        services.Scan(action =>
+        {
+            action.FromAssemblies(typeof(DependencyInjection).Assembly)
+            .AddClasses(publicOnly: false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime();
+        });
+
+        return services;
+    }
+}
